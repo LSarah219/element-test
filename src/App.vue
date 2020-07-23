56604b8e1c0a8483f1301319e5de40e2208eddc0
sarah <2640939708@qq.com>
2020-07-24 00:35:12 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form-test></form-test>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormTest from './components/FormTest.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    FormTest
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/plugins/element.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import { Form,FormItem,Input,Select,DatePicker,Col,Button,Switch,Checkbox,TimePicker,Option,CheckboxGroup,Radio,RadioGroup } from 'element-ui'
+
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Select);
+Vue.use(DatePicker);
+Vue.use(Col);
+Vue.use(Button);
+Vue.use(Switch);
+Vue.use(Checkbox);
+Vue.use(TimePicker);
+Vue.use(Option);
+Vue.use(CheckboxGroup);
+Vue.use(Radio);
+Vue.use(RadioGroup);
